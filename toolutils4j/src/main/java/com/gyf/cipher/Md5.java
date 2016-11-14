6acc3b4bfaef2dac4bd03e16475e5e22a6ce30fb
gyf <969565471@qq.com>
2016-11-14 20:44:22 +0800

    java和android工具类

---
+++ toolutils4j/src/main/java/com/gyf/cipher/Md5.java
@@ -0,0 +1,174 @@
+package com.gyf.cipher;
+
+import com.gyf.tool.StringUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * MD5 加密工具类
+ * Created by gyf on 2016/11/14.
+ */
+public class Md5 {
+    private Md5(){}
+    /**
+     * Md 5 string.
+     * md5加密通过String
+     *
+     * @param str the str
+     * @return the string
+     */
+    public static String MD5(String str) {
+        try {
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("Md5");
+            // 使用指定的字节更新摘要
+            mdInst.update(str.getBytes());
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 字节数组转换为 十六进制 数并返回
+            return StringUtil.byte2Hex(md);
+        } catch (NoSuchAlgorithmException e) {
+            return "";
+        }
+    }
+
+    /**
+     * Md 5 string.
+     * md5加密通过字节数组
+     *
+     * @param bytes the bytes
+     * @return the string
+     */
+    public static String MD5(byte[] bytes) {
+        try {
+            MessageDigest mdInst = MessageDigest.getInstance("Md5");
+            mdInst.update(bytes);
+            byte[] md = mdInst.digest();
+            return StringUtil.byte2Hex(md);
+        } catch (NoSuchAlgorithmException e) {
+            return "";
+        }
+    }
+
+    /**
+     * Md 5 string.
+     * md5加密通过输入流
+     *
+     * @param is the is
+     * @return the string
+     */
+    public static String MD5(InputStream is) {
+        byte[] dataBytes = new byte[1024];
+        int read;
+        try {
+            MessageDigest mdInst = MessageDigest.getInstance("Md5");
+            while ((read = is.read(dataBytes)) != -1) {
+                mdInst.update(dataBytes, 0, read);
+            }
+            byte[] md = mdInst.digest();
+            return StringUtil.byte2Hex(md);
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    /**
+     * Md 5 string.
+     * md5加密通过文件
+     *
+     * @param file the file
+     * @return the string
+     */
+    public static String MD5(File file) {
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            return MD5(fis);
+        } catch (FileNotFoundException e) {
+            return "";
+        }
+
+    }
+
+    /**
+     * Md 5 by path string.
+     * md5加密通过文件路径
+     *
+     * @param path the path
+     * @return the string
+     */
+    public static String MD5ByPath(String path) {
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            return MD5(fis);
+        } catch (FileNotFoundException e) {
+            return "";
+        }
+    }
+
+
+    /**
+     * Md 5 check boolean.
+     * md5检验通过字符串
+     *
+     * @param str          the str
+     * @param toBeCheckSum the to be check sum
+     * @return the boolean
+     */
+    public static boolean MD5Check(String str, String toBeCheckSum) {
+        return MD5(str).equals(toBeCheckSum);
+    }
+
+    /**
+     * Md 5 check boolean.
+     * md5检验通过字节数组
+     *
+     * @param bytes        the bytes
+     * @param toBeCheckSum the to be check sum
+     * @return the boolean
+     */
+    public static boolean MD5Check(byte[] bytes, String toBeCheckSum) {
+        return MD5(bytes).equals(toBeCheckSum);
+    }
+
+    /**
+     * Md 5 check boolean.
+     * md5检验通过输入流
+     *
+     * @param is           the is
+     * @param toBeCheckSum the to be check sum
+     * @return the boolean
+     */
+    public static boolean MD5Check(InputStream is, String toBeCheckSum) {
+        return MD5(is).equals(toBeCheckSum);
+    }
+
+    /**
+     * Md 5 check boolean.
+     * md5检验通过文件
+     *
+     * @param file         the file
+     * @param toBeCheckSum the to be check sum
+     * @return the boolean
+     */
+    public static boolean MD5Check(File file, String toBeCheckSum) {
+        return MD5(file).equals(toBeCheckSum);
+    }
+
+    /**
+     * Md 5 check boolean.
+     * md5检验通过文件路径
+     *
+     * @param path         the path
+     * @param toBeCheckSum the to be check sum
+     * @return the boolean
+     */
+    public static boolean MD5CheckByPath(String path, String toBeCheckSum) {
+        return MD5ByPath(path).equals(toBeCheckSum);
+    }
+
+}
---
+++ toolutils4j/src/main/java/com/gyf/cipher/RSA.java
@@ -0,0 +1,440 @@
+package com.gyf.cipher;
+
+
+import com.gyf.cipher.base64.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import java.io.*;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * RSA 加密解密工具类
+ * Created by gyf on 2016/11/14.
+ */
+public class RSA {
+
+    private RSA(){}
+    /**
+     * 加密算法RSA
+     */
+    public static final String KEY_ALGORITHM = "RSA";
+
+    /**
+     * 字节数据转字符串专用集合
+     */
+    public static final String MD5_SIGN = "MD5WithRSA";
+    public static final String SHA1_SIGN = "SHA1WithRSA";
+
+    /**
+     * 存储名称
+     */
+    public static final String PUBLIC_KEY_PATH = "/publicKey.RSA";
+    public static final String PRIVATE_KEY_PATH = "/privateKey.RSA";
+
+    /**
+     * 公钥的key
+     */
+    private static final String PUBLIC_KEY = "RSAPublicKey";
+
+    /**
+     * 私钥的key
+     */
+    private static final String PRIVATE_KEY = "RSAPrivateKey";
+
+    /**
+     * 生成秘钥对
+     *
+     * @return the map
+     * @throws Exception the exception
+     */
+    public static Map<String, Object> genKeyPair() throws Exception {
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+        keyPairGen.initialize(1024, new SecureRandom());
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        Map<String, Object> keyMap = new HashMap<>();
+        keyMap.put(PUBLIC_KEY, publicKey);
+        keyMap.put(PRIVATE_KEY, privateKey);
+        return keyMap;
+    }
+
+    /**
+     * 生成秘钥对
+     *
+     * @param filePath 指定存储路径
+     * @throws NoSuchAlgorithmException the no such algorithm exception
+     */
+    public static void genKeyPair(String filePath) throws Exception {
+        // KeyPairGenerator类用于生成公钥和私钥对，基于RSA算法生成对象
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+        // 初始化密钥对生成器，密钥大小为96-1024位
+        keyPairGen.initialize(1024, new SecureRandom());
+        // 生成一个密钥对，保存在keyPair中
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        // 得到私钥
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        // 得到公钥
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        try {
+            // 得到公钥字符串
+            String publicKeyString = Base64.encode(publicKey.getEncoded());
+            // 得到私钥字符串
+            String privateKeyString = Base64.encode(privateKey.getEncoded());
+
+            // 将密钥对写入到文件
+            FileWriter pubfw = new FileWriter(filePath + PUBLIC_KEY_PATH);
+            FileWriter prifw = new FileWriter(filePath + PRIVATE_KEY_PATH);
+            BufferedWriter pubbw = new BufferedWriter(pubfw);
+            BufferedWriter pribw = new BufferedWriter(prifw);
+            pubbw.write(publicKeyString);
+            pribw.write(privateKeyString);
+            pubbw.flush();
+            pubbw.close();
+            pubfw.close();
+            pribw.flush();
+            pribw.close();
+            prifw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 从文件中输入流中加载公钥
+     *
+     * @param path 地址
+     * @return 公钥字符串 string
+     * @throws Exception 加载公钥时产生的异常
+     */
+    public static String loadPublicKeyByFile(String path) throws Exception {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path + PUBLIC_KEY_PATH));
+            String readLine = null;
+            StringBuilder sb = new StringBuilder();
+            while ((readLine = br.readLine()) != null) {
+                sb.append(readLine);
+            }
+            br.close();
+            return sb.toString();
+        } catch (IOException e) {
+            throw new Exception("公钥数据流读取错误");
+        } catch (NullPointerException e) {
+            throw new Exception("公钥输入流为空");
+        }
+    }
+
+    /**
+     * 从字符串中加载公钥
+     *
+     * @param publicKeyStr 公钥数据字符串
+     * @return the rsa public key
+     * @throws Exception 加载公钥时产生的异常
+     */
+    public static RSAPublicKey loadPublicKeyByStr(String publicKeyStr)
+            throws Exception {
+        try {
+            byte[] buffer = Base64.decode(publicKeyStr);
+            KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此算法");
+        } catch (InvalidKeySpecException e) {
+            throw new Exception("公钥非法");
+        } catch (NullPointerException e) {
+            throw new Exception("公钥数据为空");
+        }
+    }
+
+    /**
+     * 从文件中加载私钥
+     *
+     * @param path 地址
+     * @return 私钥字符串 string
+     * @throws Exception the exception
+     */
+    public static String loadPrivateKeyByFile(String path) throws Exception {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path + PRIVATE_KEY_PATH));
+            String readLine;
+            StringBuilder sb = new StringBuilder();
+            while ((readLine = br.readLine()) != null) {
+                sb.append(readLine);
+            }
+            br.close();
+            return sb.toString();
+        } catch (IOException e) {
+            throw new Exception("私钥数据读取错误");
+        } catch (NullPointerException e) {
+            throw new Exception("私钥输入流为空");
+        }
+    }
+
+    /**
+     * 从字符串中加载私钥
+     *
+     * @param privateKeyStr 私钥数据字符串
+     * @return the rsa private key
+     * @throws Exception 加载公钥时产生的异常
+     */
+    public static RSAPrivateKey loadPrivateKeyByStr(String privateKeyStr) throws Exception {
+        try {
+            byte[] buffer = Base64.decode(privateKeyStr);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
+            KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+            return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此算法");
+        } catch (InvalidKeySpecException e) {
+            throw new Exception("私钥非法");
+        } catch (NullPointerException e) {
+            throw new Exception("私钥数据为空");
+        }
+    }
+
+    /**
+     * 公钥加密过程
+     *
+     * @param publicKey     公钥
+     * @param plainTextData 明文数据
+     * @return 密文字节数组 byte [ ]
+     * @throws Exception 加密过程中的异常信息
+     */
+    public static byte[] encrypt(RSAPublicKey publicKey, byte[] plainTextData) throws Exception {
+        if (publicKey == null) {
+            throw new Exception("加密公钥为空, 请设置");
+        }
+        Cipher cipher = null;
+        try {
+            // 使用默认RSA
+            cipher = Cipher.getInstance(KEY_ALGORITHM);
+            // cipher= Cipher.getInstance("RSA", new BouncyCastleProvider());
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] output = cipher.doFinal(plainTextData);
+            return output;
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此加密算法");
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InvalidKeyException e) {
+            throw new Exception("加密公钥非法,请检查");
+        } catch (IllegalBlockSizeException e) {
+            throw new Exception("明文长度非法");
+        } catch (BadPaddingException e) {
+            throw new Exception("明文数据已损坏");
+        }
+    }
+
+    /**
+     * 私钥加密过程
+     *
+     * @param privateKey    私钥
+     * @param plainTextData 明文数据
+     * @return 密文字节数组 byte [ ]
+     * @throws Exception 加密过程中的异常信息
+     */
+    public static byte[] encrypt(RSAPrivateKey privateKey, byte[] plainTextData)
+            throws Exception {
+        if (privateKey == null) {
+            throw new Exception("加密私钥为空, 请设置");
+        }
+        Cipher cipher = null;
+        try {
+            // 使用默认RSA
+            cipher = Cipher.getInstance(KEY_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+            byte[] output = cipher.doFinal(plainTextData);
+            return output;
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此加密算法");
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InvalidKeyException e) {
+            throw new Exception("加密私钥非法,请检查");
+        } catch (IllegalBlockSizeException e) {
+            throw new Exception("明文长度非法");
+        } catch (BadPaddingException e) {
+            throw new Exception("明文数据已损坏");
+        }
+    }
+
+    /**
+     * 私钥解密过程
+     *
+     * @param privateKey 私钥
+     * @param cipherData 密文数据
+     * @return 明文 byte [ ]
+     * @throws Exception 解密过程中的异常信息
+     */
+    public static byte[] decrypt(RSAPrivateKey privateKey, byte[] cipherData)
+            throws Exception {
+        if (privateKey == null) {
+            throw new Exception("解密私钥为空, 请设置");
+        }
+        Cipher cipher = null;
+        try {
+            // 使用默认RSA
+            cipher = Cipher.getInstance(KEY_ALGORITHM);
+            // cipher= Cipher.getInstance("RSA", new BouncyCastleProvider());
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] output = cipher.doFinal(cipherData);
+            return output;
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此解密算法");
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InvalidKeyException e) {
+            throw new Exception("解密私钥非法,请检查");
+        } catch (IllegalBlockSizeException e) {
+            throw new Exception("密文长度非法");
+        } catch (BadPaddingException e) {
+            throw new Exception("密文数据已损坏");
+        }
+    }
+
+    /**
+     * 公钥解密过程
+     *
+     * @param publicKey  公钥
+     * @param cipherData 密文数据
+     * @return 明文 byte [ ]
+     * @throws Exception 解密过程中的异常信息
+     */
+    public static byte[] decrypt(RSAPublicKey publicKey, byte[] cipherData)
+            throws Exception {
+        if (publicKey == null) {
+            throw new Exception("解密公钥为空, 请设置");
+        }
+        Cipher cipher = null;
+        try {
+            // 使用默认RSA
+            cipher = Cipher.getInstance(KEY_ALGORITHM);
+            // cipher= Cipher.getInstance("RSA", new BouncyCastleProvider());
+            cipher.init(Cipher.DECRYPT_MODE, publicKey);
+            byte[] output = cipher.doFinal(cipherData);
+            return output;
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此解密算法");
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InvalidKeyException e) {
+            throw new Exception("解密公钥非法,请检查");
+        } catch (IllegalBlockSizeException e) {
+            throw new Exception("密文长度非法");
+        } catch (BadPaddingException e) {
+            throw new Exception("密文数据已损坏");
+        }
+    }
+
+    /**
+     * 私钥签名
+     *
+     * @param rsaPrivateKey 私钥
+     * @param cipherData    待签名加密后的数据
+     * @return 签名后的byte[]
+     * @throws Exception 签名过程中的异常信息
+     */
+    public static byte[] sign(RSAPrivateKey rsaPrivateKey, byte[] cipherData) throws Exception {
+        try {
+            Signature signature = Signature.getInstance(MD5_SIGN);
+            signature.initSign(rsaPrivateKey);
+            signature.update(cipherData);
+            byte[] output = signature.sign();
+            return output;
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此解密算法");
+        } catch (InvalidKeyException e) {
+            throw new Exception("解密非法,请检查");
+        } catch (SignatureException e) {
+            throw new Exception("签名异常");
+        }
+    }
+
+    /**
+     * 私钥签名
+     *
+     * @param privateKey 私钥字符串
+     * @param cipherData 加密后的数据
+     * @return 签名后的byte[]
+     * @throws Exception 签名过程中的异常信息
+     */
+    public static byte[] sign(String privateKey, byte[] cipherData) throws Exception {
+        return sign(loadPrivateKeyByStr(privateKey), cipherData);
+    }
+
+    /**
+     * 公钥验签
+     *
+     * @param rsaPublicKey 公钥
+     * @param cipherData   加密后的数据
+     * @param sign         签名后的数据（base64加密）
+     * @return the boolean
+     * @throws Exception the exception
+     */
+    public static boolean verify(RSAPublicKey rsaPublicKey, byte[] cipherData, String sign) throws Exception {
+        try {
+            Signature signature = Signature.getInstance(MD5_SIGN);
+            signature.initVerify(rsaPublicKey);
+            signature.update(cipherData);
+            return signature.verify(Base64.decode(sign));
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此解密算法");
+        } catch (InvalidKeyException e) {
+            throw new Exception("解密非法,请检查");
+        } catch (SignatureException e) {
+            throw new Exception("签名异常");
+        }
+    }
+
+    /**
+     * 公钥验签
+     *
+     * @param publicKey  公钥字符串
+     * @param cipherData 加密后的数据
+     * @param sign       签名后的数据（base64加密）
+     * @return the boolean
+     * @throws Exception the exception
+     */
+    public static boolean verify(String publicKey, byte[] cipherData, String sign) throws Exception {
+        return verify(loadPublicKeyByStr(publicKey), cipherData, sign);
+    }
+
+    /**
+     * 获取私钥
+     *
+     * @param keyMap 密钥对
+     * @return string
+     */
+    public static String getPrivateKey(Map<String, Object> keyMap) {
+        Key key = (Key) keyMap.get(PRIVATE_KEY);
+        return Base64.encode(key.getEncoded());
+    }
+
+    /**
+     * 获取公钥
+     *
+     * @param keyMap 密钥对
+     * @return string
+     */
+    public static String getPublicKey(Map<String, Object> keyMap) {
+        Key key = (Key) keyMap.get(PUBLIC_KEY);
+        return Base64.encode(key.getEncoded());
+    }
+}
